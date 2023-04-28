ee684b44d0fd89c8c8f84c8b77e57d3d5e45520b
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-04-27 21:40:32 -0700

    updates

--- Basic JavaScript Projects/Project1_expressions_alert/JS/JSassignment.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/JSassignment.js
@@ -3,4 +3,6 @@ document.write(Sent1+Sent2)//Concatenated both variables
 window.alert("Welcome!")//window.alert method
 document.write(4/2)//an expression
 
-
+function displayDate() {
+    document.getElementsById("demo").innerHTML = Date();
+}
