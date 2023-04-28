a9b1de808a3b671c6258835583c4bbe55591c75d
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-04-27 20:45:31 -0700

    wrote assignments

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,3 +2,11 @@ document.write("My wife\'s name is Krizia.<br>She always tells me to \"clean you
 
 document.write("Love is patient."
     +"Love is kind."+"Love doesn\'t envy.")
+
+var A="Love is patient."
++"Love is kind."+"Love doesn\'t envy."
+
+document.write(A)
+
+var Family = "Dogs", Dad = "Corgi", Mom="Husky";
+document.write(Family)
