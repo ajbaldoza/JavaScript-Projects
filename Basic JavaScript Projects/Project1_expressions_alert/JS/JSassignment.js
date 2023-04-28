5069209e6a5dceef08cafba8a9bb8fd7db4073ed
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-04-27 21:21:29 -0700

    Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/JSassignment.js
@@ -0,0 +1,6 @@
+var Sent1="This is the beginning of the string.", Sent2="This is the end of the string."//Setting Sent1 and 2 as variables
+document.write(Sent1+Sent2)//Concatenated both variables
+window.alert("Welcome!")//window.alert method
+document.write(4/2)//an expression
+
+
