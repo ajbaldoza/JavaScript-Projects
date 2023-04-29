03d3aaaf8a747bf7261f55760423d3ac5e7e5918
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-04-29 15:27:00 -0700

    With comments

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,10 +1,10 @@
-function my_Dictionary(){
+function my_Dictionary(){ //names the function 
     var Mammal = {
         Species:'Cow',
         Color: 'White',
         Age: 6,
         Sounds: 'Moo!'
     };
-    delete Mammal.Age
-    document.getElementById('Dictionary').innerHTML = Mammal.Age;
+    delete Mammal.Age//delete statement that gets rid of value Age
+    document.getElementById('Dictionary').innerHTML = Mammal.Age;//prints the reuslt which should be undefined
 }
