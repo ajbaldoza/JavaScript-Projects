9f07f04e63c0de3e4bae43ecbd7c75ea34f1b745
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-04-29 14:36:36 -0700

    Dictionary

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,9 @@
+function my_Dictionary(){
+    var Mammal = {
+        Species:'Cow',
+        Color: 'White',
+        Age: 6,
+        Sounds: 'Moo!'
+    };
+    document.getElementById('Dictionary').innerHTML = Mammal.Age;
+}
