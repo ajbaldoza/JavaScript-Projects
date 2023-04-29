707c043f17aff7f9b67bf72a231361f318875693
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-04-29 10:23:14 -0700

    HTML event

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var mytxt = document.getElementById("txt");
+    mytxt.innerHTML = "You tapped!"
+    var yourtxt = document.getElementById("txt")
+    yourtxt.innerHTML = "You clicked!"
+}
