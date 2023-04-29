e0922355444adb530b05235697c41070e251db43
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-04-29 12:03:00 -0700

    multiply

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -2,3 +2,13 @@ function addition_function(){
     var addition = 2+2;
     document.getElementById('math').innerHTML = "2+2=" + addition;
 }
+
+function subtraction_function(){
+    var subtraction = 2-2;
+    document.getElementById('math').innerHTML = "2-2=" + subtraction;
+}
+
+function multiplication(){
+    var simple_Math = 2*2;
+    document.getElementById('math').innerHTML = "2*2=" + simple_Math;
+}
