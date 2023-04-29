9851b9d86c08645c26f74a917d0f0a07b70dfd87
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-04-29 12:21:09 -0700

    Multiple math op

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -12,3 +12,8 @@ function multiplication(){
     var simple_Math = 2*2;
     document.getElementById('math').innerHTML = "2*2=" + simple_Math;
 }
+
+function more_Math(){
+    var simple_Math = (2+2) * 10/2-5;
+    document.getElementById('math').innerHTML = "2 plus 2, multiplied by 10,divided in half and then subtracted by 5 equals " + simple_Math;
+}
