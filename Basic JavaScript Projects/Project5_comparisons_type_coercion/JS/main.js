cf197b419cc0c11dd5bab22833378d91b4b58aa7
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-04-29 16:53:34 -0700

    ===

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -12,3 +12,22 @@ document.write(-10E400)
 document.write(22>23)
 
 console.log(2>2)
+
+document.write(2==1+1)
+
+//triple equals
+X = 10
+Y = 5+5
+document.write(X===Y)
+
+J = 5
+K = "ten" 
+document.write(J===K)
+
+m = 'five'
+n = 5
+document.write(m===n)
+
+u = 'five'
+i = 'ten'
+document.write(u===i)
